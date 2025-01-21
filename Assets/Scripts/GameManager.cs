ebb2e0c3a17ca85016cd079640ae0e8fd8a50bba
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2025-01-21 17:50:49 -0400

    Imported New Shader and UI Assets
    Imported a new shader for the holograms and UI assets for application UI.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,24 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("UI")]
-    [SerializeField] private GameObject regularUI;
-    [SerializeField] private GameObject infoUI;
+    
 
     public void Quit()
     {
         Application.Quit();
     }
 
-    public void SwitchToInfo()
-    {
-        regularUI.SetActive(false);
-        infoUI.SetActive(true);
-    }
-
-    public void SwitchToReg()
-    {
-        regularUI.SetActive(true);
-        infoUI.SetActive(false);
-    }
+    
 }
